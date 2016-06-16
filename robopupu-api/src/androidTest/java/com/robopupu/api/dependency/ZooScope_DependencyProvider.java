86a01561707333fc28feb12171fd5ccc27090277
Marko Salmela <marko.salmela@fuusio.org>
2016-06-16 20:27:47 +0300

    Fixed a unit test.

--- robopupu-api/src/androidTest/java/com/robopupu/api/dependency/ZooScope_DependencyProvider.java
+++ robopupu-api/src/androidTest/java/com/robopupu/api/dependency/ZooScope_DependencyProvider.java
@@ -6,19 +6,19 @@ public class ZooScope_DependencyProvider extends DependencyProvider {
     @Override
     protected <T> void getDependencies(final DependencyQuery<T> query) {
 
-        if (query.matches(Elephant.class)) {
+        if (query.matches(Elephant.class, Elephant.class)) {
             if (query.add((T)new Elephant())) {
                 return;
             }
         }
 
-        if (query.matches(Lion.class)) {
+        if (query.matches(Lion.class, Lion.class)) {
             if (query.add((T)new Lion())) {
                 return;
             }
         }
 
-        if (query.matches(Monkey.class)) {
+        if (query.matches(Monkey.class, Monkey.class)) {
             final Banana banana = D.get(Banana.class);
             if (query.add((T)new Monkey(banana))) {
                 return;
